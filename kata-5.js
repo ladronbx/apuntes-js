b606fafb8787513d7f1f60abddc25081a1a6264f
ladronbx <ladronbravovlc@gmail.com>
2023-08-12 14:59:52 +0200

    kata 5

---
+++ kata-5.js
@@ -0,0 +1,9 @@
+
+var n = prompt("De qué numero quieres la tabla de multiplicar?",0);
+var multiplicador = 0;
+var x = prompt("La tabla comenzará a multiplicar desde el número 0. Escribe hasta qué número : ");
+
+while(multiplicador <= x){
+    document.write("<h5>"+n+" * "+multiplicador+" = "+(n*multiplicador));
+    ++ multiplicador;
+}
